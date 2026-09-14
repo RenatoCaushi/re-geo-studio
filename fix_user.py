262df8aaa9f985bc243cf2ce4f79d381a25b3cde
Renato Caushi <ren.caushi22@gmail.com>
2026-09-14 13:11:32 +0200

    Fix python path in fix_user script

--- fix_user.py
+++ fix_user.py
@@ -1,12 +1,20 @@
 import os
-import django
+import sys
+from pathlib import Path
+
+# Shto dosjen kryesore në Python Path
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.append(str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+import django
 django.setup()
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+# Krijon ose përditëson përdoruesin
 user, created = User.objects.get_or_create(username='Geologyrenni')
 user.set_password('otanerstudio')
 user.is_superuser = True
@@ -14,4 +22,7 @@ user.is_staff = True
 user.is_active = True
 user.save()
 
-print("Përdoruesi u përditësua me sukses!")
+if created:
+    print("Përdoruesi u krijua me sukses!")
+else:
+    print("Fjalëkalimi u përditësua me sukses!")
