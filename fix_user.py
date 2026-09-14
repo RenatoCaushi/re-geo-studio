076a836b84e60230e19c75ca91ab97bdb5d3beea
Renato Caushi <ren.caushi22@gmail.com>
2026-09-14 14:05:38 +0200

    Update fix_user for both upper and lower case username

--- fix_user.py
+++ fix_user.py
@@ -7,11 +7,14 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-u, created = User.objects.get_or_create(username='Geologyrenni')
-u.set_password('otanerstudio')
-u.is_superuser = True
-u.is_staff = True
-u.is_active = True
-u.save()
 
-print("ADMIN_PASSWORD_RESET_SUCCESSFUL")
+# Krijon ose përditëson të dyja variantet e username-it
+for username in ['Geologyrenni', 'geologyrenni']:
+    u, created = User.objects.get_or_create(username=username)
+    u.set_password('otanerstudio')
+    u.is_superuser = True
+    u.is_staff = True
+    u.is_active = True
+    u.save()
+
+print("BOTH_USERS_RESET_SUCCESSFULLY")
