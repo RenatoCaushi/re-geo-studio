4445c04a21f46d5cf185e80e384fa35fa12df563
Renato Caushi <ren.caushi22@gmail.com>
2026-09-14 14:13:43 +0200

    Read superuser credentials from Render environment variables

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,26 @@
+import os
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_default_superuser(sender, **kwargs):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    
+    # Lexon nga Environment Variables të Render-it ose përdor vlerat default
+    username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'Geologyrenni')
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'otanerstudio')
+    
+    u, created = User.objects.get_or_create(username=username)
+    u.set_password(password)
+    u.is_superuser = True
+    u.is_staff = True
+    u.is_active = True
+    u.save()
+    print(f"--> SUPERUSER {username} U PËRDITËSUA ME SUKSES TE BAZA E TË DHËNAVE!")
 
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        post_migrate.connect(create_default_superuser, sender=self)
