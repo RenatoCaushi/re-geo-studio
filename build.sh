3ef5873fad33a339b6f6d0e1f7bcfe72e24b6fd9
Renato Caushi <ren.caushi22@gmail.com>
2026-09-14 13:19:02 +0200

    Configure superuser creation directly inside build.sh

--- build.sh
+++ build.sh
@@ -6,15 +6,19 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input || true
 python manage.py migrate
 
-# Krijon ose përditëson përdoruesin direkt gjatë build-it
-python manage.py shell -c "
+# Krijimi i superuser-it përmes shell-it të vetë Django-s
+python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
-u, _ = User.objects.get_or_create(username='Geologyrenni')
-u.set_password('otanerstudio')
-u.is_superuser = True
-u.is_staff = True
-u.is_active = True
-u.save()
-print('Përdoruesi u ruajt me sukses!')
-"
+if not User.objects.filter(username='Geologyrenni').exists():
+    User.objects.create_superuser('Geologyrenni', 'admin@example.com', 'otanerstudio')
+    print("Superuser u me sukses!")
+else:
+    u = User.objects.get(username='Geologyrenni')
+    u.set_password('otanerstudio')
+    u.is_superuser = True
+    u.is_staff = True
+    u.is_active = True
+    u.save()
+    print("Fjalëkalimi u përditësua me sukses!")
+EOF
