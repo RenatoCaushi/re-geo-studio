8da9cdc260315b7f0717cfe90620fd814742a26f
Renato Caushi <ren.caushi22@gmail.com>
2026-09-14 10:37:57 +0200

    Add fix_user script to create admin user automatically

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python populate.py
+
+python fix_user.py
