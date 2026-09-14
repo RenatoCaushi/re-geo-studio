931a2b6261a21d672e18424d66c8f1d5aca24adc
Renato Caushi <ren.caushi22@gmail.com>
2026-09-14 13:17:28 +0200

    Embed superuser logic in build script

--- build.sh
+++ build.sh
@@ -2,5 +2,19 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+python manage.py collectstatic --no-input || true
 python manage.py migrate
+
+# Krijon ose përditëson përdoruesin direkt gjatë build-it
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+u, _ = User.objects.get_or_create(username='Geologyrenni')
+u.set_password('otanerstudio')
+u.is_superuser = True
+u.is_staff = True
+u.is_active = True
+u.save()
+print('Përdoruesi u ruajt me sukses!')
+"
