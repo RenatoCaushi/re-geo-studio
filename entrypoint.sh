1ee9d2b9a20cc54aa5d858aee89c7bca00419ba9
Renato Caushi <ren.caushi22@gmail.com>
2026-09-17 15:00:25 +0200

    Fix entrypoint to avoid auto seeding on restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
-# Ekzekutohet gjithmonë kur ndizet serveri
-python manage.py migrate
-
-python manage.py shell << 'EOF'
-from django.contrib.auth import get_user_model
-User = get_user_model()
-u, created = User.objects.get_or_create(username='Geologyrenni')
-u.set_password('otanerstudio')
-u.is_superuser = True
-u.is_staff = True
-u.is_active = True
-u.save()
-print("ADMIN_CREATED_SUCCESSFULLY")
-EOF
-
-# Nis serverin gunicorn
+# Ekzekuto vetëm gunicorn pa prekur bazën e të dhënave
 exec gunicorn config.wsgi:application
